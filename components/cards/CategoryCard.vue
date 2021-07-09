700bb1d7f5bf5894d211640a29bb03586c473788
CeoNabux <lualreye@gmail.com>
2021-07-09 02:03:53 -0500

    categories added

--- components/cards/CategoryCard.vue
+++ components/cards/CategoryCard.vue
@@ -1,5 +1,23 @@
 <template>
   <div class="w-full p-2">
-    Aqui va la carta
+    <div class="w-full p-2 bg-gray-300">
+      <div class="grid grid-col-3 grid-row-3">
+        <p class="text-blue-800 text-2xl col-start-1">
+          Este es un titulo
+        </p>
+        <p-button name="Más..." class="bg-gray-300 text-blue-600 shadow row-start-2 row-span-1 w-28 h-7 mt-3 text-base" />
+        <figure class="col-start-2 col-span-2 row-start-2 row-span-2">
+          <img :src="image" alt="category-tool">
+        </figure>
+      </div>
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  data: () => ({
+    image: require("@/assets/general/atornillador.png")
+  })
+}
+</script>
