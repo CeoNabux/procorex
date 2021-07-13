b03e9775bd0fac0a122bd1311d4fab48568eb9da
CeoNabux <lualreye@gmail.com>
2021-07-13 11:59:27 -0500

    button template fixed

--- components/TheProducts.vue
+++ components/TheProducts.vue
@@ -1,25 +1,15 @@
 <template>
-  <div class="w-full max-w-screen-xl mt-4 mx-auto">
+  <div class="w-full max-w-screen-xl mt-16 mx-auto">
     <div class="w-full flex flex-col justify-center items-center mx-auto">
       <p class=" mx-auto text-4xl text-blue-900 font-bold mb-6">
         Últimos productos
       </p>
-      <div class="w-full flex justify-between items-center mx-auto overflow-x-auto">
-        <div class="w-full lg:w-1/3">
-          <product-card />
-        </div>
-        <div class="w-full lg:w-1/3">
-          <product-card />
-        </div>
-        <div class="w-full lg:w-1/3">
-          <product-card />
-        </div>
-        <div class="w-full lg:w-1/3">
-          <product-card />
-        </div>
-        <div class="w-full lg:w-1/3">
-          <product-card />
-        </div>
+      <div class="w-full px-2 overflow-x-scroll flex justify-between mx-auto py-4">
+        <product-card />
+        <product-card />
+        <product-card />
+        <product-card />
+        <product-card />
       </div>
     </div>
   </div>
--- components/cards/ProductCard.vue
+++ components/cards/ProductCard.vue
@@ -1,5 +1,55 @@
 <template>
-  <div class="w-full p-2">
-    Este es un producto
+  <div class="w-72 flex-none bg-gray-50 py-2 shadow mr-4 card">
+    <div class="w-full relative">
+      <span class="bg-yellow-500 absolute top-0 px-2 py-1 text-base text-white rounded-xl">Venta</span>
+      <figure class="w-full my-1 p-2">
+        <img :src="image" alt="tool" srcset="">
+      </figure>
+      <p class="text-blue-900 font-medium text-lg">
+        Atornillador inalámbrico
+      </p>
+      <p class="text-blue-900 font-light font-sm">
+        Black N Decker DE 4.8 V
+      </p>
+      <div class="w-full mt-4 flex justify-center items-center">
+        <div class="w-1/2 h-12">
+          <p-button name="Pedir" class="aparecer h-12 bg-yellow-500 text-white text-base" />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  data: () => ({
+    image: require("@/assets/general/atornillador.png")
+  })
+}
+</script>
+
+<style scoped>
+.aparecer {
+  animation-name: appear moveUp;
+  animation-delay: 100ms;
+  animation-duration: 3000ms;
+  animation-fill-mode: forwards;
+}
+
+@keyframes appear {
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+}
+@keyframes moveUp {
+  0% {
+    transform: translateY(-20px);
+  }
+  100% {
+    transform: translateY(0px)
+  }
+}
+</style>
