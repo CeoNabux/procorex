e740364bc7850db79bd45c31039d69b5eb7f5f1a
CeoNabux <lualreye@gmail.com>
2021-07-14 12:48:16 -0500

    category card fixed

--- components/cards/CategoryCard.vue
+++ components/cards/CategoryCard.vue
@@ -5,8 +5,8 @@
         <p class="text-blue-800 text-2xl col-start-1">
           Este es un titulo
         </p>
-        <p-button name="Más..." class="bg-gray-100 text-blue-600 shadow row-start-2 row-span-1 w-28 h-7 mt-3 text-base" />
-        <figure class="col-start-2 col-span-3 row-start-2 row-span-3">
+        <p-button name="Más..." class="bg-gray-100 text-blue-600 shadow row-start-2 row-span-1 w-28 h-12 mt-8 text-base" />
+        <figure class="col-start-2 col-span-3 row-start-2 row-span-2">
           <img :src="image" alt="category-tool">
         </figure>
       </div>
