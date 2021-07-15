a6cbdaa5588771af8b1d2f97c79d416e67996c67
CeoNabux <lualreye@gmail.com>
2021-07-15 13:14:25 -0500

    post added and ready to bind

--- components/cards/PostPreview.vue
+++ components/cards/PostPreview.vue
@@ -1,14 +1,14 @@
 <template>
-  <div class="w-72 h-80 shadow-lg bg-image rounded-md py-2" :style="{backgroundImage: 'url(' + bgImage + ')'}">
+  <div class="w-72 h-80 shadow-lg bg-image rounded-md py-2 px-3" :style="{backgroundImage: 'url(' + bgImage + ')'}">
     <div class="w-full h-full flex flex-col justify-between">
-      <p class="font-semibold text-gray-200 text-right">
+      <p class="font-semibold text-gray-300 text-right">
         12-12-21
       </p>
       <div class="w-full">
         <p class="text-2xl font-bold text-white">
           Maquinas poderosas
         </p>
-        <p class="font-semibold text-gray-200 text-left">
+        <p class="font-medium text-gray-300 text-left">
           Aprende a usar las mejores herramientas...
         </p>
       </div>
