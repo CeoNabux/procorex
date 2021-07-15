cc8b39d621b25fcc43de763d80672a676c01ca80
CeoNabux <lualreye@gmail.com>
2021-07-15 13:12:43 -0500

    post card fixed

--- components/ThePostsSection.vue
+++ components/ThePostsSection.vue
@@ -2,24 +2,13 @@
   <div class="w-full max-w-screen-xl mx-auto mt-16">
     <div class="w-full flex flex-col justify-center items-center mx-auto">
       <p class=" mx-auto text-4xl text-blue-900 font-bold mb-10">
-        Conoce nuestros últimos productos
+        Últimas noticias
       </p>
       <div class="w-full flex flex-wrap justify-center lg:justify-between items-center mx-auto">
-        <div class="w-full sm:w-1/2 lg:w-1/5">
-          <post-preview />
-        </div>
-        <div class="w-full sm:w-1/2 lg:w-1/5">
-          <post-preview />
-        </div>
-        <div class="w-full sm:w-1/2 lg:w-1/5">
-          <post-preview />
-        </div>
-        <div class="w-full sm:w-1/2 lg:w-1/5">
-          <post-preview />
-        </div>
-        <div class="w-full sm:w-1/2 lg:w-1/5">
-          <post-preview />
-        </div>
+        <post-preview />
+        <post-preview />
+        <post-preview />
+        <post-preview />
       </div>
     </div>
   </div>
@@ -33,3 +22,4 @@ export default {
   }
 }
 </script>
+    PostPreview
--- components/cards/PostPreview.vue
+++ components/cards/PostPreview.vue
@@ -1,5 +1,33 @@
 <template>
-  <div>
-    Esto es una tarjeta de prueba
+  <div class="w-72 h-80 shadow-lg bg-image rounded-md py-2" :style="{backgroundImage: 'url(' + bgImage + ')'}">
+    <div class="w-full h-full flex flex-col justify-between">
+      <p class="font-semibold text-gray-200 text-right">
+        12-12-21
+      </p>
+      <div class="w-full">
+        <p class="text-2xl font-bold text-white">
+          Maquinas poderosas
+        </p>
+        <p class="font-semibold text-gray-200 text-left">
+          Aprende a usar las mejores herramientas...
+        </p>
+      </div>
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  data: () => ({
+    bgImage: require('@/assets/general/pintura-no-exif-tinified.jpg')
+  })
+}
+</script>
+
+<style scoped>
+.bg-image {
+  background-position: center;
+  background-size: cover;
+  background-repeat: no-repeat;
+}
+</style>
