aac1fd0379f4c070b58dae79d21644e906172672
CeoNabux <lualreye@gmail.com>
2021-07-19 16:43:45 -0500

    blog page template rendenring with storyblok

--- components/cards/PostPreview.vue
+++ components/cards/PostPreview.vue
@@ -1,16 +1,18 @@
 <template>
-  <a :href="`blog/${id}`" class="w-72 h-80 shadow-lg bg-image rounded-md py-2 px-3 my-4 mx-2 lg:m-1" :style="{backgroundImage: 'url(' + image + ')'}">
-    <div class="w-full h-full flex flex-col justify-between">
-      <p class="font-semibold text-gray-300 text-right">
-        {{ time }}
-      </p>
-      <div class="w-full">
-        <p class="text-2xl font-bold text-white">
-          {{ title }}
+  <a :href="`blog/${id}`" class="w-72 h-80 shadow-lg bg-image rounded-md" :style="{backgroundImage: 'url(' + image + ')'}">
+    <div class="w-full h-full flex flex-col justify-between bg-gradient-to-r from-indigo-800">
+      <div class="py-2 px-3 my-4 mx-2 lg:m-1 w-full h-full flex flex-col justify-between ">
+        <p class="font-semibold text-gray-300 text-right">
+          {{ time }}
         </p>
-        <p class="font-medium text-gray-300 text-left">
-          {{ description }}
-        </p>
+        <div class="w-full">
+          <p class="text-2xl font-bold text-white">
+            {{ title }}
+          </p>
+          <p class="font-medium text-gray-300 text-left">
+            {{ description }}
+          </p>
+        </div>
       </div>
     </div>
   </a>
--- pages/blog.vue
+++ pages/blog.vue
@@ -2,17 +2,23 @@
   <div class="max-w-screen-xl">
     <div class="flex flex-col justify-between items-center">
       <!-- LAST POST -->
-      <div class="w-full flex flex-col justify-center items-center bg-image" :style="{backgroundImage: 'url(' + post[0].image + ')'}">
-        <p class="text-4xl text-white font-bold text-center">
-          {{ post[0].title }}
-        </p>
-        <p class="text-center text-gray-200 font-semibold">
-          {{ post[0].description }}
-        </p>
-        <a :href="`/blog/${post[0].id}`" class="w-14 h-10 flex justify-center items-center bg-white text-gray-900" >
-          Leer más
-          <p-icon name="arrowRight" class="text-yellow-600" />
-        </a>
+      <div class="w-full flex justify-center items-center bg-image mb-16" :style="{backgroundImage: 'url(' + post[0].image + ')'}">
+        <div class="w-full flex-col justify-center items-center bg-gradient-to-r from-yellow-900 py-8">
+          <p class="text-4xl text-white font-bold text-center">
+            {{ post[0].title }}
+          </p>
+          <p class="text-center text-gray-200 font-semibold my-8">
+            {{ post[0].description }}
+          </p>
+          <a :href="`/blog/${post[0].id}`" class="w-36 h-10 rounded mx-auto flex justify-evenly items-center bg-white" >
+            <p class="text-gray-800 text-base">
+              Leer más
+            </p>
+            <div class="w-4 h-4">
+              <p-icon name="arrowRight" class="text-yellow-600" />
+            </div>
+          </a>
+        </div>
       </div>
       <!-- GENERAL POSTS -->
       <div class="w-full flex flex-wrap justiy-center items-center">
@@ -62,7 +68,5 @@ export default {
   background-position: center;
   background-repeat: no-repeat;
   background-size: cover;
-  background-color: linear-gradient(241deg, rgba(255,255,255,0.7679446778711485) 0%, rgba(0,13,191,0.896796218487395) 100%);
-
 }
 </style>
