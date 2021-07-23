0adc3709794512764e0d9cfa14c48dfe7f1cc19f
CeoNabux <lualreye@gmail.com>
2021-07-23 17:56:54 -0500

    finxing vue-pdf

--- pages/catalogos/_catalogueId/index.vue
+++ pages/catalogos/_catalogueId/index.vue
@@ -1,31 +1,62 @@
 <template>
   <div>
-    <clien-only>
+    <p class="text-xl">{{ title }}</p>
+    <client-only>
+      <div
+        v-if="loadedRatio > 0 && loadedRatio < 1"
+        style="background-color: green; color: white; text-align: center"
+        :style="{ width: loadedRatio * 100 + '%' }"
+      >
+        {{ Math.floor(loadedRatio * 100) }}%
+      </div>
       <pdf
+        v-if="file"
         class="border border-red-500"
         :src="file"
-        :page="1"
+        ref="pdf"
+        :page="page"
+        :rotate="rotate"
+        @password="password"
+        @progress="loadedRatio = $event"
+        @error="error"
+        @num-pages="numPages = $event"
+        @link-clicked="page = $event"
       />
-    </clien-only>
+    </client-only>
   </div>
 </template>
 
-
 <script>
 export default {
+  data: () => ({
+    show: true,
+    loadedRatio: 0,
+    page: 1,
+    numPage: 0,
+    rotate: 0
+  }),
   asyncData(context) {
     return context.app.$storyapi
-      .get('cdn/stories/catalogos/' + context.params.postId, {
-        version: process.env.NODE_ENV == 'production' ? 'published' : 'draft'
+      .get("cdn/stories/catalogos/" + context.params.catalogueId, {
+        version: process.env.NODE_ENV == "production" ? "published" : "draft"
       })
       .then(res => {
+        console.log(res);
         return {
           image: res.data.story.content.image.filename,
           title: res.data.story.content.title,
           file: res.data.story.content.archivo.filename,
           description: res.data.story.description
-        }
-      })
+        };
+      });
+  },
+  methods: {
+    password: function(updatePassword, reason) {
+      updatePassword(promt('password is "test"'));
+    },
+    error: function(err) {
+      console.log(err);
+    }
   }
-}
+};
 </script>
