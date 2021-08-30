a7f17c28a7c7d46d900973b072ec4a64f4d6e9f1
CeoNabux <lualreye@gmail.com>
2021-08-30 01:08:09 -0500

    public folder added to static folder

--- pages/catalogos/_catalogueId/index.vue
+++ pages/catalogos/_catalogueId/index.vue
@@ -1,41 +1,11 @@
 <template>
   <div>
-    <p class="text-xl">{{ title }}</p>
-    <client-only>
-      <div
-        v-if="loadedRatio > 0 && loadedRatio < 1"
-        style="background-color: green; color: white; text-align: center"
-        :style="{ width: loadedRatio * 100 + '%' }"
-      >
-        {{ Math.floor(loadedRatio * 100) }}%
-      </div>
-      <pdf
-        v-if="file"
-        class="border border-red-500"
-        :src="local"
-        ref="pdf"
-        :page="page"
-        :rotate="rotate"
-        @password="password"
-        @progress="loadedRatio = $event"
-        @error="error"
-        @num-pages="numPages = $event"
-        @link-clicked="page = $event"
-      />
-    </client-only>
+    Este seria mi catalogo
   </div>
 </template>
 
 <script>
 export default {
-  data: () => ({
-    show: true,
-    loadedRatio: 0,
-    page: 1,
-    numPage: 0,
-    rotate: 0,
-    local: "http://localhost:3000/assets/pdf/linea-de-productos.pdf"
-  }),
   asyncData(context) {
     return context.app.$storyapi
       .get("cdn/stories/catalogos/" + context.params.catalogueId, {
@@ -51,14 +21,6 @@ export default {
           description: res.data.story.description
         };
       })
-  },
-  methods: {
-    password: function(updatePassword, reason) {
-      updatePassword(promt('password is "test"'));
-    },
-    error: function(err) {
-      console.log(err);
-    }
   }
 };
 </script>
