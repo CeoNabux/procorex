edf40fb6a4eb5a441b0ab88eb710c8e0e451aa8f
CeoNabux <lualreye@gmail.com>
2021-08-30 18:22:33 -0500

    first letter capitalize in categories

--- pages/productos/index.vue
+++ pages/productos/index.vue
@@ -4,19 +4,29 @@
       Productos
     </h2>
     <div class="max-w-screen-xl flex justiy-between items-start w-full">
-      <div class="flex justiy-between items-center w-full lg:w-1/5">
-        <p class="text-blue-900 text-base">
-          Filtros
-        </p>
-        <div class="w-6 h-6">
-          <p-icon name="filter" class="text-blue-900" />
+      <div class="w-full lg:w-1/5">
+        <div class="flex flex-start align-items mb-4">
+          <p class="text-blue-900 text-base mr-2">
+            Filtros
+          </p>
+          <div class="w-6 h-6">
+            <p-icon name="filter" class="text-blue-900" />
+          </div>
+        </div>
+        <div class="flex flex-col w-full">
+          <button
+            v-for="(categoria, i) in categorias"
+            :key="i"
+            class="text-gray-400 font-semibold text-lg mb-2"
+            @click="filtroPorCategoria"
+          >
+            <p class="w-full text-left">{{ categoria.categoria }}</p>
+          </button>
         </div>
       </div>
       <!-- AQUI VA LA SECCIONH DE PRODUCTOS -->
       <div class="w-full lg:w-4/5">
-        <div
-          class="w-full px-2 flex flex-wrap justify-between mx-auto py-4"
-        >
+        <div class="w-full px-2 flex flex-wrap justify-between mx-auto py-4">
           <div v-for="(item, i) in productos" :key="i">
             <product-card
               :title="item.title"
@@ -32,31 +42,40 @@
 </template>
 
 <script>
-import ProductCard from "@/components/cards/ProductCard.vue"
+import ProductCard from "@/components/cards/ProductCard.vue";
 
 export default {
   components: {
     ProductCard
   },
-  asyncData(context) {
-    return context.app.$storyapi
-      .get("cdn/stories", {
-        version: context.isDev ? "draft" : "published",
-        starts_with: "productos/"
-      })
-      .then(res => {
-        console.log(res);
+  async asyncData({ app }) {
+    const productos = await app.$storyapi.get("cdn/stories", {
+      starts_with: "productos/"
+    });
+    const categorias = await app.$storyapi.get("cdn/stories", {
+      starts_with: "categorias/"
+    });
+    return {
+      productos: productos.data.stories.map(pr => {
         return {
-          productos: res.data.stories.map(ct => {
-            return {
-              id: ct.slug,
-              title: ct.content.title,
-              image: ct.content.image.filename,
-              description: ct.content.description.slice(0, 40)
-            };
-          })
+          id: pr.slug,
+          title: pr.content.title,
+          image: pr.content.image.filename,
+          categoria: pr.content.categorias,
+          description: pr.content.description.slice(0, 40)
         };
-      });
+      }),
+      categorias: categorias.data.stories.map(categoria => {
+        return {
+          categoria: categoria.content.name.charAt(0).toUpperCase() + categoria.content.name.slice(1)
+        };
+      })
+    };
+  },
+  methods: {
+    filtroPorCategoria() {
+      console.log(this.categoria.categoria)
+    }
   }
 };
 </script>
