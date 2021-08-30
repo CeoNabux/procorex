ca5e5cdc11d0575b3879a2a51dc0a80e58071558
CeoNabux <lualreye@gmail.com>
2021-08-30 02:33:12 -0500

    catalogue need to be fixed

---
+++ components/DocViewer.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <pdf :src='pdfDoc' />
+  </div>
+</template>
+
+<script>
+import pdf from 'vue-pdf'
+
+export default {
+  components: {
+    pdf
+  },
+  props: {
+    pdfDoc: {
+      type: String,
+      required: null
+    }
+  }
+}
+</script>
+
--- pages/catalogos/_catalogueId/index.vue
+++ pages/catalogos/_catalogueId/index.vue
@@ -1,25 +1,28 @@
 <template>
   <div>
     Este seria mi catalogo
+    <pdf src="@/assets/pdf/linea-de-productos.pdf" />
   </div>
 </template>
 
 <script>
+import pdf from 'vue-pdf'
+import DocViewer from '../../../components/DocViewer.vue';
 export default {
+  components: {
+    DocViewer,
+    pdf
+  },
   asyncData(context) {
-    return context.app.$storyapi
+      return context.app.$storyapi
       .get("cdn/stories/catalogos/" + context.params.catalogueId, {
         version: process.env.NODE_ENV == "production" ? "published" : "draft"
       })
       .then(res => {
-        console.log(res);
         console.log(res.data.story.content.archivo.filename)
         return {
-          image: res.data.story.content.image.filename,
-          title: res.data.story.content.title,
-          file: res.data.story.content.archivo.filename,
-          description: res.data.story.description
-        };
+          pdf: res.data.story.content.archivo.filename
+        }
       })
   }
 };
