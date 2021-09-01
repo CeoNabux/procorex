d58667e9ab18d1fe622c6859aa9d32290658c1fa
CeoNabux <lualreye@gmail.com>
2021-09-01 09:52:20 -0500

    footer template changed

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="bg-gray-200 mb-16 md:mb-0">
+  <div class="bg-gray-200 py-4 mb-16 md:mb-0">
     <div class="max-w-screen-xl flex justify-end items-center pr-12">
       <p class="text-base text-green-700 font-medium">
         Con cariño del equipo <a href="www.maverick.ec" class="underline">Maverick</a> 💚
