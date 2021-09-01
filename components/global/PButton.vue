68e31136e1034fc4089814ad396abe001c7c8727
CeoNabux <lualreye@gmail.com>
2021-09-01 13:39:23 -0500

    text in button change to white

--- components/global/PButton.vue
+++ components/global/PButton.vue
@@ -1,5 +1,5 @@
 <template>
-  <button class="w-full text-xl rounded-xl font-semibold text-blue-600" @click="$emit('click', $event)">
+  <button class="w-full text-xl rounded-xl font-semibold text-white" @click="$emit('click', $event)">
     {{name}}
   </button>
 </template>
