da4643ee05ed8f60cabf37f917d0f4db6062fafb
CeoNabux <lualreye@gmail.com>
2021-08-31 19:05:16 -0500

    white space added to catalogue section

--- components/TheCatalogues.vue
+++ components/TheCatalogues.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="w-full max-w-screen-xl mx-auto mt-16">
+  <div class="w-full max-w-screen-xl mx-auto mt-24">
     <div class="w-full flex flex-col justify-center items-center mx-auto">
       <p class=" mx-auto text-4xl text-blue-900 font-bold mb-10">
         Chequea nuestro catálogo
       </p>
       <div class="w-full flex flex-wrap justify-center lg:justify-between items-center mx-auto">
-        <div v-for="(item, i) in array" :key="i" class="w-full lg:w-1/2">
+        <div v-for="(item, i) in array" :key="i" class="w-full px-2 lg:w-1/2 lg:px-0">
           <catalogue-card :bgImage='item.bgImage' :title='item.title' :link='item.link' />
         </div>
       </div>
