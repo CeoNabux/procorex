64b2c5594138164a430acc3d6ef198a3d6bdc92f
CeoNabux <lualreye@gmail.com>
2021-09-01 13:33:02 -0500

    blog template fixed

--- pages/blog/_postId/index.vue
+++ pages/blog/_postId/index.vue
@@ -1,25 +1,34 @@
 <template>
   <div>
-    <div class="bg-image" :style="{backgroundImage: `url(` + image + ')' }">
-      <div class="w-full h-80 flex flex-col justify-end items-start bg-gradient-to-t from-green-900 pl-4">
-        <p class="text-left max-w-screen-xl text-white font-bold text-3xl md:text-4xl lg:text-5xl mb-4">
+    <div class="bg-image" :style="{ backgroundImage: `url(` + image + ')' }">
+      <div
+        class="w-full h-80 flex flex-col justify-end items-start bg-gradient-to-t from-green-900 pl-4"
+      >
+        <div class="w-full max-w-screen-xl mx-auto">
+          <p
+          class="text-left mr-auto text-white font-bold text-3xl md:text-4xl lg:text-5xl mb-4"
+        >
           {{ title }}
         </p>
-      </div>
-    </div>
-    <div class=" w-full flex flex-col max-w-screen-xl pl-4">
-        <p class="text-left text-gray-800 font-bold text-base md:text-lg mb-2 mt-6">
-          Publicado en: <span class="text-yellow-800">{{ date.slice(0, 10) }}</span>
-        </p>
-        <div class="flex flex-wrap mt-16">
-          <sb-rich-text :text='body' />
         </div>
       </div>
+    </div>
+    <div class=" w-full flex flex-col max-w-screen-xl pl-4 mx-auto">
+      <p
+        class="text-left text-gray-800 font-bold text-base md:text-lg mb-2 mt-6"
+      >
+        Publicado en:
+        <span class="text-yellow-800">{{ date.slice(0, 10) }}</span>
+      </p>
+      <div class="flex flex-wrap mt-16">
+        <sb-rich-text :text="body" />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import SbRichText from '@/components/SbRichText.vue'
+import SbRichText from "@/components/SbRichText.vue";
 
 export default {
   components: {
@@ -27,15 +36,15 @@ export default {
   },
   computed: {
     richtext() {
-      return typeof this.text === 'string'
+      return typeof this.text === "string"
         ? this.text
-        : this.$storyapi.richTextResolver.render(this.text)
+        : this.$storyapi.richTextResolver.render(this.text);
     }
   },
   asyncData(context) {
     return context.app.$storyapi
-      .get('cdn/stories/blog/' + context.params.postId, {
-        version: process.env.NODE_ENV == 'production' ? 'published' : 'draft'
+      .get("cdn/stories/blog/" + context.params.postId, {
+        version: "published"
       })
       .then(res => {
         return {
@@ -43,10 +52,10 @@ export default {
           title: res.data.story.content.title,
           body: res.data.story.content.body,
           date: res.data.story.published_at
-        }
-      })
+        };
+      });
   }
-}
+};
 </script>
 
 <style scoped>
@@ -56,5 +65,3 @@ export default {
   background-repeat: no-repeat;
 }
 </style>
-
-
