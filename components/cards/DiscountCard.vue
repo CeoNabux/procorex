0ca479775804e80370af426ecd4b6093a80e2e2d
CeoNabux <lualreye@gmail.com>
2021-11-11 19:24:28 -0500

    discount card template fixed

--- components/cards/DiscountCard.vue
+++ components/cards/DiscountCard.vue
@@ -1,36 +1,24 @@
 <template>
   <div class="w-full p-2 my-12">
-    <div
-      class="w-full bg-image rounded-md shadow-md"
-      :style="{ backgroundImage: 'url(' + bgImage + ')' }"
-    >
-      <div class="grid grid-cols-5 grid-rows-2">
-        <div class="flex justify-start flex-col col-start-1 col-span-3 row-start-1 row-span-1 p-1">
-          <p class="text-xl md:text-3xl font-bold text-gray-300">
-            {{ title }}
-          </p>
-          <p
-            class="text-light my-2 md:my-4 lg:my-8 text-lg md:text-2xl text-blue-300"
-          >
-            {{ discount }}% Dcto.
-          </p>
-        </div>
-        <div class="col-start-1 col-span-3 flex justify-center items-center row-start-2 row-span-1">
-          <div class="rounded w-full h-full mx-auto flex items-center">
-            <button
-              name="Detalle"
-              class="bg-green-500 w-full flex justify-center items-center h-1/2 lg:h-16 text-white py-2 px-4 rounded-lg text-sm focus:outline-none"
-              @click="redirection"
-            >
-              Más Información
-              <div class="w-8 h-8 ml-2">
-                <p-icon name="whatsApp" class="text-white" />
-              </div>
-            </button>
-          </div>
-        </div>
-      </div>
+    <div class="w-full rounded-t-xl bg-yellow-500 py-1 px-2">
+      <p class="text-white text-lg text-center">{{ title }}</p>
     </div>
+    <div class="bg-white h-2 w-full" />
+    <div
+      class="bg-image shadow-md h-72 w-full"
+      :style="{ backgroundImage: 'url(' + bgImage + ')' }"
+    ></div>
+    <div class="bg-white h-2 w-full" />
+    <button
+      name="Detalle"
+      class="bg-green-400 w-full flex justify-center items-center h-12 text-white px-4 rounded-b-lg text-sm focus:outline-none"
+      @click="redirection"
+    >
+      Más Información
+      <div class="w-6 h-6 ml-2">
+        <p-icon name="whatsApp" class="text-white" />
+      </div>
+    </button>
   </div>
 </template>
 
@@ -55,7 +43,9 @@ export default {
   },
   methods: {
     redirection() {
-      window.open(`https://api.whatsapp.com/send?phone=593996525883&text=%F0%9F%98%80%20Hola!%20vengo%20de%20su%20web%20y%20quisiera%20saber%20m%C3%A1s%20sobre%20+${this.title}`)
+      window.open(
+        `https://api.whatsapp.com/send?phone=593996525883&text=%F0%9F%98%80%20Hola!%20vengo%20de%20su%20web%20y%20quisiera%20saber%20m%C3%A1s%20sobre%20+${this.title}`
+      );
     }
   }
 };
