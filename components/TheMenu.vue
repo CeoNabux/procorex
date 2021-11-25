f0c0c7cc6d0bd56fc71edf4ac40105992583dd0c
CeoNabux <lualreye@gmail.com>
2021-11-25 12:57:39 -0500

    the menu component

--- components/TheMenu.vue
+++ components/TheMenu.vue
@@ -5,12 +5,12 @@
         <nav class="w-full px-2">
           <ul class="w-full flex flex-col justify-between items-center">
             <div class="w-full flex justify-end items-center">
-              <button class="w-8 h-8 rounded-full bg-blue-600 p-2 focus:outline-none" @click="closeMenu">
+              <button class="w-8 h-8 rounded-full bg-yellow-500 p-2 focus:outline-none" @click="closeMenu">
                 <p-icon name='close' class="text-blue-50"></p-icon>
               </button>
             </div>
             <li class="flex justify-between items-center cursor-pointer my-4 w-full" v-for="(item, i) in menu" :key="i">
-              <nuxt-link class="text-yellow-600 text-lg font-semibold" :to="item.path">
+              <nuxt-link class="text-yellow-500 text-lg font-bebas" :to="item.path">
                 {{item.name}}
               </nuxt-link>
               <span class="w-6 h-6">
@@ -18,8 +18,8 @@
               </span>
             </li>
             <div class="w-full flex items-center mt-4">
-              <p class="text-xl text-blue-600 font-semibold">
-                Te recordamos que puedes visitarnos en <a class="text-xl font-bold underline" href="https://www.google.com/maps/dir//Ferreteria+Industrial+Procorex+S.A.,+Guayaquil+090606/@-2.1268001,-79.929434,17z/data=!4m8!4m7!1m0!1m5!1m1!1s0x902d72a4bae55287:0xc6ac4af353505699!2m2!1d-79.929548!2d-2.1256179" target="_blank">Nuestras oficinas</a>
+              <p class="text-xl text-blue-600 font-bebas">
+                Te recordamos que puedes visitarnos en <a class="text-xl font-bebas text-yellow-500 underline" href="https://www.google.com/maps/dir//Ferreteria+Industrial+Procorex+S.A.,+Guayaquil+090606/@-2.1268001,-79.929434,17z/data=!4m8!4m7!1m0!1m5!1m1!1s0x902d72a4bae55287:0xc6ac4af353505699!2m2!1d-79.929548!2d-2.1256179" target="_blank">Nuestras oficinas</a>
               </p>
             </div>
           </ul>
